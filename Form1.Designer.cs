cd4deace886909d81d4c9277da8b80720ac507c3
Artem <Artem@DESKTOP-6LJUG5I>
2022-04-19 23:08:44 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Pressure_regulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(3, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(980, 756);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(972, 730);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Сборка";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(270, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Откройте Inventor!";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(160, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(160, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Версия Inventor";
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(6, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Открыть Inventor";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(530, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(435, 718);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::Pressure_regulator.Properties.Resources.Сборка;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(429, 534);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(974, 734);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Деталь 1";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 761);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Регулятор давления";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// Вставка библиотек
+using Inventor;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace Pressure_regulator
+{
+    public partial class Form1 : Form
+    {
+        public string Версия_Inventor = "2022";
+        /// <summary>
+        /// ThisApplication - Объект для определения активного состояния Инвентора
+        /// </summary>
+        private Inventor.Application ThisApplication = null;
+        /// <summary>
+        /// Словарь для хранения ссылок на документы деталей
+        /// </summary>
+        private Dictionary<string, PartDocument> oPartDoc = new Dictionary<string,
+        PartDocument>();
+        /// <summary>
+        /// Словарь для хранения ссылок на определения деталей
+        /// </summary>
+        private Dictionary<string, PartComponentDefinition> oCompDef = new Dictionary<string,
+        PartComponentDefinition>();
+        /// <summary>
+        /// Словарь для хранения ссылок на инструменты создания деталей
+        /// </summary>
+        private Dictionary<string, TransientGeometry> oTransGeom = new Dictionary<string,
+        TransientGeometry>();
+        /// <summary>
+        /// Словарь для хранения ссылок на транзакции редактирования
+        /// </summary>
+        private Dictionary<string, Transaction> oTrans = new Dictionary<string, Transaction>();
+        /// <summary>
+        /// Словарь для хранения имен сохраненных документов деталей
+        /// </summary>
+        private Dictionary<string, string> oFileName = new Dictionary<string, string>();
+        private AssemblyDocument oAssemblyDocName;
+        private AssemblyComponentDefinition oAssCompDef;
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = Версия_Inventor;
+            try
+            {
+                //Проверка наличия активного состояния Инвентора.
+                ThisApplication = (Inventor.Application)System.Runtime.InteropServices.
+                Marshal.GetActiveObject("Inventor.Application");
+                if (ThisApplication != null) label2.Text = "Инвентор открыт!";
+                
+            }
+            catch
+            {
+                // Если Инвентор не открыт, то возвращаемся в основную программу.
+                label2.Text = "Откройте Инвентор!";
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Запуск Инвентора может занять несколько минут!");
+            System.Diagnostics.Process.Start("C:/Program Files/Autodesk/Inventor " + Версия_Inventor +
+            "/Bin/Inventor.exe");
+            label2.Text = "Инвентор открыт!";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int число = 0;
+            Версия_Inventor = textBox1.Text;
+
+            число = Convert.ToInt16(textBox1.Text);
+            if (число >= 2009)
+                if (число >= 2015 && число <= 2022)
+                {
+                    MessageBox.Show("Принята версия Инвентора " + textBox1.Text);
+                }
+                else
+                {
+                    if (число < 2015) MessageBox.Show("Используется устаревшая версия Инвентора! " +
+                    textBox1.Text);
+                    if (число > 2022)
+                    {
+                        MessageBox.Show("Данная версия Инвентора " + textBox1.Text +
+                        " не существует!\nПо умолчанию будет принята версия Инвентора 2022 !");
+                        Версия_Inventor = "2022";
+                        textBox1.Text = Версия_Inventor;
+                    }
+                }
+            else if (число >= 2000)
+            {
+                MessageBox.Show("Используется устаревшая версия Инвентора " + textBox1.Text +
+                " !\nПо умолчанию будет принята версия Инвентора 2022 !");
+                Версия_Inventor = "2022";
+                textBox1.Text = Версия_Inventor;
+            }
+        }
+    }
+}
